cb8a17c5c9a676922c770498e4201f0c56612552
swa6file <tguys004@gmail.com>
2025-09-14 13:32:07 +0700

    Добавлено описание Модели Работника !

--- Model/Worker.cs
+++ Model/Worker.cs
@@ -8,6 +8,14 @@ namespace Model
 {
     public class Worker
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public int Age {  get; set; }
+
+        public string Brigade { get; set; }
+
+        public string Salary { get; set; }
     }
 }
